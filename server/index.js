89c788522f5388653e637df51644d6c2b3893710
Chris McVey <chris.mcvey@gmail.com>
2021-03-24 10:50:23 -0700

    Very basic load balancer implemented

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 
-const app = express();
+
+
+const servers = ['http://ec2-18-219-66-39.us-east-2.compute.amazonaws.com:3000', 'http://ec2-18-217-240-208.us-east-2.compute.amazonaws.com:3000'];
+
+let currentServer = 0;
+
+const handler = (req, res) => {
+  req.pipe(request({ url: servers[currentServer] + req.url })).pipe(res);
+  currentServer = (currentServer + 1) % servers.length;
+}
+
+const app = express().get('*', handler).post('*', handler).put('*', handler);
 const port = 3000;
 
 app.use(bodyParser.json());
